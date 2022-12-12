323a0d98d1f6a7144932f198dfe8ce90097d29e7
윤형식 <puhahaha3000@naver.com>
2022-12-12 17:48:50 +0900

    init project
    Signed-off-by: 윤형식 <puhahaha3000@naver.com>

---
+++ src/main/java/com/example/japanego/JapanegoApplication.java
@@ -0,0 +1,13 @@
+package com.example.japanego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanegoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapanegoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/japanego/JapanegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.japanego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapanegoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
