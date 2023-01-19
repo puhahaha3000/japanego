4747ea199c3bda256d60b28bc826e5cf6d294485
임현수 <qnddj0213@gmail.com>
2023-01-19 20:35:41 +0900

    오류 수정
    WordBankRelationMapper : Mapper Annotation 누락된 것 추가
    WordBankRelationServiceImpl :　Autowired -> 생성자 주입방식으로 변경
    Signed-off-by: ThinkingMoomin <qnddj0213@gmail.com>

--- src/main/java/com/example/japanego/mapper/WordBankRelationMapper.java
+++ src/main/java/com/example/japanego/mapper/WordBankRelationMapper.java
@@ -2,7 +2,9 @@ package com.example.japanego.mapper;
 
 import com.example.japanego.vo.MemberWordRelationVo;
 import com.example.japanego.vo.WordWordBankRelationVo;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface WordBankRelationMapper {
 
     void insertWord(WordWordBankRelationVo wordWordBankRelationVo);
--- src/main/java/com/example/japanego/service/WordBankRelationServiceImpl.java
+++ src/main/java/com/example/japanego/service/WordBankRelationServiceImpl.java
@@ -4,15 +4,17 @@ import com.example.japanego.mapper.WordBankRelationMapper;
 import com.example.japanego.vo.MemberWordRelationVo;
 import com.example.japanego.vo.WordWordBankRelationVo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class WordBankRelationServiceImpl implements WordBankRelationService {
 
-    @Autowired
-    private WordBankRelationMapper wordBankRelationMapper;
+    private final WordBankRelationMapper wordBankRelationMapper;
+
+    public WordBankRelationServiceImpl(WordBankRelationMapper wordBankRelationMapper) {
+        this.wordBankRelationMapper = wordBankRelationMapper;
+    }
 
     @Override
     public void addWordToWordBank(WordWordBankRelationVo wordWordBankRelationVo) {
